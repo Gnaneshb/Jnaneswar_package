d5c615ff838739e479abf986a621ac0206ea9120
Gnaneshb <gnanesh.bammidi320@gmail.com>
2018-07-09 18:35:23 +0530

    package name updated

--- Jnaneswar_pacakge/setup.py
+++ Jnaneswar_pacakge/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Jnaneswar_mypackage",
+    name="Jnaneswar_package",
     version="0.0.1",
     author="Gnanesh",
     author_email="gnanesh72892@gmail.com",
