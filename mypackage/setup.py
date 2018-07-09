4800f67beba45b6002b2fad45286fc0511801b53
Gnaneshb <gnanesh.bammidi320@gmail.com>
2018-07-09 17:35:31 +0530

    updated url of package

--- mypackage/setup.py
+++ mypackage/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/example-project",
+    url="https://github.com/Gnaneshb/mypackage.git",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
